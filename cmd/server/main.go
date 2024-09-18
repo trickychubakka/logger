6765143e330dea91038170063d83c452ff76a507
Tricky.chubakka <farfrom@gmail.com>
2024-09-18 14:30:46 +0300

    iter1 commit 4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,42 +20,42 @@ var store = MemStorage{gaugeMap: make(map[string]float64), counterMap: make(map[
 // Константа для кодирования смысла полей после парсинга URL на основе их порядкового номера
 // Пример: localhost:8080/update/gauge/metric2/777.4
 const (
-	update  = 0
-	gauge   = 1
-	counter = 1
-	metric  = 2
-	value   = 3
+	//update  = 0
+	//gauge   = 1
+	//counter = 1
+	metric = 2
+	value  = 3
 )
 
 func urlToMap(url string) ([]string, error) {
 	// Формируем слайс компонентов URL, предварительно отрезав крайние "/"
-	splittedUrl := strings.Split(strings.Trim(url, "/"), "/")
+	splittedURL := strings.Split(strings.Trim(url, "/"), "/")
 	fmt.Println("Trim in action", strings.Trim(url, "/"))
 	// Если длина разобранного URL не больше 2-х -- недостаток указания метрики/значения, возвращаем StatusNotFound
-	if len(splittedUrl) <= 3 {
+	if len(splittedURL) <= 3 {
 		fmt.Println("ERROR: URL is too short")
-		return splittedUrl, errors.New("URL is too short")
+		return splittedURL, errors.New("URL is too short")
 	}
 	// Если длина разобранного URL больше 4 -- в URL что-то лишнее
-	if len(splittedUrl) > 4 {
+	if len(splittedURL) > 4 {
 		fmt.Println("ERROR: URL is too long")
-		return splittedUrl, errors.New("URL is too long")
+		return splittedURL, errors.New("URL is too long")
 	}
-	fmt.Println("urlToMap:", splittedUrl)
-	return splittedUrl, nil
+	fmt.Println("urlToMap:", splittedURL)
+	return splittedURL, nil
 }
 
 func gaugeMetricHandler(w http.ResponseWriter, r *http.Request) {
-	splittedUrl, err := urlToMap(r.URL.String())
+	splittedURL, err := urlToMap(r.URL.String())
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	fmt.Println("URL.String is:", r.URL.String(), "\nsplittedUrl is:", splittedUrl, "len is:", len(splittedUrl))
-	if s, err := strconv.ParseFloat(splittedUrl[value], 64); err == nil {
+	fmt.Println("URL.String is:", r.URL.String(), "\nsplittedURL is:", splittedURL, "len is:", len(splittedURL))
+	if s, err := strconv.ParseFloat(splittedURL[value], 64); err == nil {
 		// записываем метрику в хранилище
-		fmt.Println("metric is:", splittedUrl[metric], "\nvalue is:", splittedUrl[value])
-		store.gaugeMap[splittedUrl[metric]] = s
+		fmt.Println("metric is:", splittedURL[metric], "\nvalue is:", splittedURL[value])
+		store.gaugeMap[splittedURL[metric]] = s
 		// Формируем ответ
 		w.Header().Set("content-type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
@@ -68,20 +68,16 @@ func gaugeMetricHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func counterMetricHandler(w http.ResponseWriter, r *http.Request) {
-	splittedUrl, err := urlToMap(r.URL.String())
+	splittedURL, err := urlToMap(r.URL.String())
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	fmt.Println("URL.String is:", r.URL.String(), "\nsplittedUrl is:", splittedUrl, "len is:", len(splittedUrl))
-	if s, err := strconv.ParseInt(splittedUrl[value], 10, 64); err == nil {
+	fmt.Println("URL.String is:", r.URL.String(), "\nsplittedURL is:", splittedURL, "len is:", len(splittedURL))
+	if s, err := strconv.ParseInt(splittedURL[value], 10, 64); err == nil {
 		// записываем метрику в хранилище
-		fmt.Println("metric is:", splittedUrl[metric], "\nvalue is:", splittedUrl[value])
-		if _, ok := store.counterMap[splittedUrl[metric]]; ok {
-			store.counterMap[splittedUrl[metric]] += s
-		} else {
-			store.counterMap[splittedUrl[metric]] = s
-		}
+		fmt.Println("metric is:", splittedURL[metric], "\nvalue is:", splittedURL[value])
+		store.counterMap[splittedURL[metric]] += s
 		// Формируем ответ
 		w.Header().Set("content-type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
