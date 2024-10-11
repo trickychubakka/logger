0d9511ac48433ee481b4eb0b118c4c04677a530a
Tricky.chubakka <farfrom@gmail.com>
2024-10-12 01:37:49 +0300

    Iter6 refactoring commit 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"logger/internal/handlers"
+	"logger/internal/logging"
 )
 
 var conf Config
+var sugar zap.SugaredLogger
 
 // flagTest флаг режима тестирования для отключения парсинга командной строки при тестировании
 var flagTest = false
@@ -15,6 +18,16 @@ func main() {
 
 	//var conf Config
 
+	// создаём предустановленный регистратор zap
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		// вызываем панику, если ошибка
+		panic(err)
+	}
+	defer logger.Sync()
+	// делаем регистратор SugaredLogger
+	sugar = *logger.Sugar()
+
 	if err := initConfig(&conf); err != nil {
 		panic(err)
 	}
@@ -22,11 +35,15 @@ func main() {
 	//gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
+	router.Use(logging.WithLogging(&sugar))
+	//router.GET("/", logging.WithLogging(handlers.GetAllMetrics, &sugar))
 	router.GET("/", handlers.GetAllMetrics)
+	//router.POST("/update/:metricType/:metricName/:metricValue", logging.WithLogging(handlers.MetricsHandler, &sugar))
 	router.POST("/update/:metricType/:metricName/:metricValue", handlers.MetricsHandler)
+	//router.GET("/value/:metricType/:metricName", logging.WithLogging(handlers.GetMetric, &sugar))
 	router.GET("/value/:metricType/:metricName", handlers.GetMetric)
 
-	err := router.Run(conf.runAddr)
+	err = router.Run(conf.runAddr)
 	if err != nil {
 		panic(err)
 	}
