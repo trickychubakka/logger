2ccf97baf59ca16579a7079c57dcad61a8dfdc42
Tricky.chubakka <farfrom@gmail.com>
2024-11-29 01:24:53 +0300

    Iter15 commit 4 goroutines

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -16,95 +16,81 @@ import (
 // FlagTest флаг режима тестирования для отключения парсинга командной строки при тестировании
 var FlagTest = false
 
-// Run функция выполнения цикла polling-а метрик
-func runOLD(myMetrics internal.MetricsStorage, _ *conf.AgentConfig) {
-
-	for {
-		for i := 0; i < config.ReportInterval; i = i + config.PollInterval {
-			if err := internal.MetricsPolling(&myMetrics); err != nil {
-				log.Println(err)
-			}
-			log.Println("\nmetrics:", myMetrics)
-			time.Sleep(time.Duration(config.PollInterval) * time.Second)
-		}
-
-		log.Println("run. SendMetricsJSONBatch start. myMetrics is:", myMetrics)
-		if err := internal.SendMetricsJSONBatch(&myMetrics, "http://"+config.Address+"/updates", &config); err != nil {
-			log.Println("main: error from SendMetricsJSONBatch:", err)
-			log.Panicf("%s", errors.Unwrap(err))
-		}
-	}
-}
-
+// metricsPolling функция сбора метрик
 func metricsPolling(m *sync.RWMutex, myMetrics *internal.MetricsStorage, config *conf.AgentConfig) error {
 	log.Println("start metricsPolling goroutine")
 	for {
-		m.RLock()
+		log.Println("metricsPolling goroutine polling")
+		m.Lock()
 		if err := internal.MetricsPolling(myMetrics); err != nil {
 			log.Println("error in metricsPolling :", err)
 			return err
 		}
-		m.RUnlock()
-		log.Println("\nmetrics:", myMetrics)
+		m.Unlock()
 		time.Sleep(time.Duration(config.PollInterval) * time.Second)
 	}
 }
 
+// gopsMetricsPolling функция сбора метрик, собранных через gopsutil
 func gopsMetricsPolling(m *sync.RWMutex, myMetrics *internal.MetricsStorage, config *conf.AgentConfig) error {
 	log.Println("start gopsMetricsPolling goroutine")
 	for {
-		m.RLock()
+		log.Println("gopsMetricsPolling goroutine polling")
+		m.Lock()
 		if err := internal.GopsMetricPolling(myMetrics); err != nil {
 			log.Println("error in metricsPolling :", err)
 			return err
 		}
-		m.RUnlock()
-		log.Println("\nmetrics:", myMetrics)
+		m.Unlock()
 		time.Sleep(time.Duration(config.PollInterval) * time.Second)
 	}
 }
 
-func metricsReport(myMetrics *internal.MetricsStorage, config *conf.AgentConfig) error {
+// metricReport функция отсылки метрик на сервер
+func metricsReport(m *sync.RWMutex, myMetrics *internal.MetricsStorage, config *conf.AgentConfig) error {
 	log.Println("start metricsReport goroutine")
 	for {
 		time.Sleep(time.Duration(config.ReportInterval) * time.Second)
 		log.Println("run. SendMetricsJSONBatch start. myMetrics is:", myMetrics)
+		m.RLock()
 		if err := internal.SendMetricsJSONBatch(myMetrics, "http://"+config.Address+"/updates", config); err != nil {
 			log.Println("main: error from SendMetricsJSONBatch:", err)
 			log.Panicf("%s", errors.Unwrap(err))
 		}
+		m.RUnlock()
 	}
 }
 
+// Run функция запуска горутин polling-а метрик и их отсылки на сервер
 func run(myMetrics internal.MetricsStorage, config *conf.AgentConfig) {
 	var m sync.RWMutex
 
 	go func() {
 		err := metricsPolling(&m, &myMetrics, config)
 		if err != nil {
-			log.Panicf("%s", errors.Unwrap(err))
+			log.Panicf("metricsPolling error %s", errors.Unwrap(err))
 		}
 	}()
 
 	go func() {
 		err := gopsMetricsPolling(&m, &myMetrics, config)
 		if err != nil {
-			log.Panicf("%s", errors.Unwrap(err))
+			log.Panicf("gopsMetricsPolling error %s", errors.Unwrap(err))
 		}
 	}()
 
 	log.Println("start metricsReport")
 	go func() {
-		err := metricsReport(&myMetrics, config)
+		err := metricsReport(&m, &myMetrics, config)
 		if err != nil {
-			log.Panicf("%s", errors.Unwrap(err))
+			log.Panicf("metricsReport error %s", errors.Unwrap(err))
 		}
 	}()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
-	log.Println("AGENT STOPPED!!!")
+	log.Println("AGENT STOPPED.")
 	os.Exit(1)
 
 }
