f7d28be469a55a403961be09d292e48902501f2e
Tricky.chubakka <farfrom@gmail.com>
2025-02-03 17:43:01 +0300

    Iter20 commit1

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -872,7 +872,6 @@ func ExampleGetMetricJSON_statusNotFound() {
 	conf := &initconf.Config{
 		Key: "superkey",
 	}
-
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest(http.MethodPost, "/value/", bytes.NewReader([]byte(requestedMetric)))
 	c := SetTestGinContext(w, r)
